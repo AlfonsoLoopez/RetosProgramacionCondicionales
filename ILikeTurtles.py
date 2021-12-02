9f51a08daf1d65bff059af22f2f11204d02b8ca6
AlfonsoLoopez <92704004+AlfonsoLoopez@users.noreply.github.com>
2021-12-02 12:25:51 -0600

    Reto #4
    “I like turtles”
    Escribe un programa que pida al usuario ingrese su animal favorito, si coloca ‘Tortuga’, ‘tortuga’, ‘TORTUGA’ o cualquier otra variante de la palabra entonces mostrar en pantalla “También me gustan las tortugas”. En caso contrario mostrar el mensaje “Ese animal es genial, pero prefiero las tortugas”.

---
+++ ILikeTurtles.py
@@ -0,0 +1,8 @@
+print("Ingrese su animal favorito: ")
+aniPref = input()
+aniPref = aniPref.upper()
+
+if(aniPref == "TORTUGA" or aniPref== "TORTUGAS"):
+    print("A mi también me gustan las tortugas. ")
+else:
+    print("Ese animal es genial, pero prefiero las tortugas.")
